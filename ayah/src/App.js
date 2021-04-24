d6399dc1ba167c0cba46a20a37709c99f091921b
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-04-24 14:00:11 +0100

    finished

--- ayah/src/App.js
+++ ayah/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Ayah from './components/Ayah';
 
 
--- ayah/src/components/Ayah.jsx
+++ ayah/src/components/Ayah.jsx
@@ -3,24 +3,22 @@ import axios from 'axios'
 import background from '../images/image2.jpg'
 
 
-import Backdrop from '@material-ui/core/Backdrop';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import { makeStyles } from '@material-ui/core/styles';
+// import { makeStyles } from '@material-ui/core/styles';
 
 
 
-const useStyles = makeStyles((theme) => ({
-  backdrop: {
-    zIndex: theme.zIndex.drawer + 1,
-    color: '#fff',
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   backdrop: {
+//     zIndex: theme.zIndex.drawer + 1,
+//     color: '#fff',
+//   },
+// }));
 
 
 
 const Ayah = () => {
 
-  const classes = useStyles();
+  // const classes = useStyles();
 
   const [ayah, setAyah] = useState([''])
 
@@ -38,20 +36,8 @@ const Ayah = () => {
   const urlEnglish = `https://api.alquran.cloud/ayah/${ayahNumb}/en.asad`
   const urlArabic = `https://api.alquran.cloud/ayah/${ayahNumb}`
 
-  useEffect(() => { 
-    axios.all([
-      axios.get(urlArabic),
-      axios.get(urlEnglish)
-    ])
-    .then(axios.spread((urlArabic,  urlEnglish) => {
-        setAyah(urlArabic.data.data);
-        // console.log(urlArabic.data.data)
-        setSurah(urlArabic.data.data.surah);
-        setEng(urlEnglish.data.data);
-        // console.log(urlEnglish.data.data);
-      }))
-  },  []);
 
+  
   const refreshPage = () => {
     axios.all([
       axios.get(urlArabic),
@@ -66,23 +52,38 @@ const Ayah = () => {
 
   }
 
+  useEffect(() => { 
+    axios.all([
+      axios.get(urlArabic),
+      axios.get(urlEnglish)
+    ])
+    .then(axios.spread((urlArabic,  urlEnglish) => {
+        setAyah(urlArabic.data.data);
+  
+        setSurah(urlArabic.data.data.surah);
+        setEng(urlEnglish.data.data);
+      }))
+  },  [urlArabic, urlEnglish]);
+
+
 
   return (
 
 
-    <div class="bg-sky flex items-center justify-center min-h-screen bg-gray-600 ">
+    <div class="bg-sky flex items-center justify-center min-h-screen ">
       
+   
        
       <div class="max-w-5xl  p-4 m-6  rounded-3xl shadow-xl  	"  style={{ backgroundImage: `url('${background}')` }}>
 
 
         <div class="bg-fixed">  
+        
 
 
           <div class="flex flex-col space-y-4 h-full justify-between">
-        
-            <div className="text-base text-1xl font-medium text-white text-center 	"> {surah.name}
-
+           
+            <div className="text-base text-1xl font-medium text-white text-center 	"> {surah.name} 
 
               <div className="font-mono text-base text-xs font-small text-white text-center	">  {surah.englishName} - {surah.englishNameTranslation}  </div>
               <div class="pt-6"></div>
@@ -102,7 +103,7 @@ const Ayah = () => {
 
 
                 <div class="pt-8"></div>
-                <h1 class="text-center  font-mono text-xs text-center text-white	"> Revealed In {surah.revelationType}  </h1>
+                <h1 class="text-center  font-mono text-xs text-center text-white	">  {surah.revelationType} Ayah </h1>
 
     
               </div >
@@ -113,6 +114,10 @@ const Ayah = () => {
             <button type="button" onClick={refreshPage}> <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="white">
   <path fill-rule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd" />
 </svg> </button>
+
+<p class="text-black text-opacity-25 ... font-mono text-sm text-centre ">Aayah.app</p>
+
+
           </div>
         </div>
       </div>
