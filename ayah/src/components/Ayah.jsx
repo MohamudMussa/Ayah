2d58da1e1b0e89ad9dfbc01efc40cf9dbc4c97d9
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-04-28 14:02:02 +0100

    purple

--- ayah/src/components/Ayah.jsx
+++ ayah/src/components/Ayah.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import background from '../images/image9.jfif'
+import background from '../images/image3.jpg'
 import ReactAudioPlayer from 'react-audio-player';
 
 import ReactGA from 'react-ga';
