1adb36c70c310310d4bc66462a28a4485bc66fcb
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-04-28 14:09:19 +0100

    Update Ayah.jsx

--- ayah/src/components/Ayah.jsx
+++ ayah/src/components/Ayah.jsx
@@ -120,7 +120,7 @@ const Ayah = () => {
       </Backdrop> */}
                 <div class="pt-6"></div>
                 
-                <h5 onClick={refreshPage} class="text-base  font-mono font-medium text-xs text-white	 text-center	"> - {eng.text ? eng.text : 'Click the refresh icon or anywhere to reveal an Ayah'}  </h5>
+                <h5 onClick={refreshPage} class="text-base  font-mono font-medium text-xs text-white	 text-center	"> - {eng.text ? eng.text : 'Click the refresh icon below to reveal an Ayah'}  </h5>
 
 
                 <div class="pt-8"></div>
