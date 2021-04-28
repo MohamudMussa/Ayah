79f9e5c0a390a512531199cb796b70ebca66be33
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-04-28 02:03:39 +0100

    update

--- ayah/src/components/Search.jsx
+++ ayah/src/components/Search.jsx
@@ -36,9 +36,10 @@ const Search = () => {
             </Modal.Header>
             <Modal.Body>
 
-                <lable> Surah </lable>
+               
 
                 <input 
+                  placeholder="Enter Surah Number"
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
 
                 type="text"
