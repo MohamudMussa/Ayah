639c1c96ebf08f8fcbe3975e8116d83dde70c322
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-04-28 01:54:42 +0100

    form handle complete

--- ayah/src/components/Search.jsx
+++ ayah/src/components/Search.jsx
@@ -9,6 +9,12 @@ const Search = () => {
     const [surah, setSurah] = useState('')
     const [aayah, setAayah] = useState('')
 
+    const print = () => {
+ 
+        console.log(`Surah number is: ${surah}`)
+        console.log(`Aayah number is: ${aayah}`)
+    }
+
     return (
         <>
           <button onClick={() => setSmShow(true)}>
@@ -30,25 +36,27 @@ const Search = () => {
             </Modal.Header>
             <Modal.Body>
 
-            <Form class="text-black text-opacity-25 ... font-mono text-sm text-centre" >
-  <Form.Row>
-    <Col>
-      <Form.Control placeholder="Suran Numb"
-      value={setSurah}
-      onChange={(e) => {setSurah(e.target.value)}} />
-    </Col>
-    <Col>
-      <Form.Control placeholder="Ayah Numb" />
-    </Col>
-  </Form.Row>
-</Form>
+                <lable> Surah </lable>
 
-<Button >
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20"fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 16l2.879-2.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242zM21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-</svg>
-              </Button>
+                <input 
+                type="text"
+                value={surah}
+                onChange={(e) => {setSurah(e.target.value)}}
+              
 
+                />
+
+                <lable> Aayah </lable>
+              
+                <input type="text"
+                value={aayah}
+                onChange={(e) => {setAayah(e.target.value)}}
+                
+                />
+                <button onClick={print}> test </button>
+            
+
+            
 
             </Modal.Body>
           </Modal>
